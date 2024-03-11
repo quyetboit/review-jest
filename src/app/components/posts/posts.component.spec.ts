d19f901f438406bbf270fea1e7ce069594024c66
quyetboit <quyetboit@gmail.com>
2024-03-11 22:48:04 +0700

    test for list post

--- src/app/components/posts/posts.component.spec.ts
+++ src/app/components/posts/posts.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostsComponent } from './posts.component';
+import { of } from 'rxjs';
+import { Post } from '../../models';
+import { PostService } from '../../services/post.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+
+const MOCK_POSTS: Post[] = [
+  { id: 1, title: "Title 1", body: "Body 1", userId: 1},
+  { id: 2, title: "Title 2", body: "Body 2", userId: 2},
+  { id: 3, title: "Title 3", body: "Body 3", userId: 3},
+  { id: 4, title: "Title 4", body: "Body 4", userId: 4},
+  { id: 5, title: "Title 5", body: "Body 5", userId: 5},
+]
+
+const postServiceSpy = {
+  getPosts: jest.fn().mockReturnValue(of(MOCK_POSTS))
+}
 
 describe('PostsComponent', () => {
   let component: PostsComponent;
@@ -8,8 +25,22 @@ describe('PostsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PostsComponent]
+      imports: [
+        PostsComponent,
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule,
+      ],
     })
+    .overrideComponent(
+      PostsComponent,
+      {
+        set: {
+          providers: [
+            { provide: PostService, useValue: postServiceSpy },
+          ]
+        }
+      }
+    )
     .compileComponents();
     
     fixture = TestBed.createComponent(PostsComponent);
@@ -20,4 +51,14 @@ describe('PostsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("onInit", () => {
+    it("should get posts", () => {
+      expect(postServiceSpy.getPosts).toHaveBeenCalled();
+    })
+
+    it("should have posts", () => {
+      expect(component.posts).toEqual(MOCK_POSTS);
+    })
+  })
 });
